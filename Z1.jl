08749a1b9fc12681e5410bac5e6ed5bebefb8819
chwilko <249761@stuent.pwr.edu.pl>
2022-03-06 17:26:36 +0100

    Dla Ciebie Andzej

--- Z1.jl
+++ Z1.jl
@@ -1,5 +1,11 @@
 using Plots #mega długo się ładuje 
 
+
+MC = 10000;
+k = 12;
+len = 1000;
+
+
 function CDE(t, X, MC)
     X = sort(X, dims=1);
     X = X * ones(1,length(t));
@@ -15,9 +21,15 @@ end
 #     # return (sum(X .< t' .* ones(MC,1), dims = 1))';
 # end
 
-MC = 10000;
-k = 12;
-len = 1000;
+function characterist_r_i(t, X, MC)
+    X = X * ones(1,length(t));
+    t = t' .* ones(MC,1);
+    Re = (sum(cos.(X .* t), dims = 1) ./ (MC * ones(1,len)))';
+    Im = (sum(sin.(X .* t), dims = 1) ./ (MC * ones(1,len)))';
+    return [Re Im];
+end
+
+
 
 t = range(-3.5, 3.5, length=len);
 X = sum(rand(MC, k), dims=2) - 12 * 1/2* ones(MC,1);
@@ -28,6 +40,10 @@ histogram(X, legend = false, normalize = true)
 CDE_X = CDE(t,X, MC);
 
 plot(t[2:length(t)], CDE_X)
+
+phi = characterist_r_i(t, X, MC);
+
+
 
 # print("\n");
 
