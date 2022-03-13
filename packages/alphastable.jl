b3c4a29c18fb9a7b82b4d0aa506870e6d8b537d3
chwilko <249761@stuent.pwr.edu.pl>
2022-03-13 02:41:39 +0100

    poprawione On na .+ ect, w trakcie naprawiania PDF

--- packages/alphastable.jl
+++ packages/alphastable.jl
@@ -1,37 +1,34 @@
 include("MBP.jl")
 
 function alphastable_(N, M, alpha, beta)
-    On = ones(N,M);
-    U = pi * (rand(N,M) - (1 / 2 * On));
+    U = pi * (rand(N,M) .- (1 / 2));
     W = d_exp(N, M, 1);
     if alpha == 1
         X = 2 / pi * (
-            (On * pi / 2 + beta * U) .* tan.(U) -
-            beta * log.(W .* cos.(U) ./ (On + beta * U)));
+            (pi / 2 .+ beta * U) .* tan.(U) -
+            beta * log.(W .* cos.(U) ./ (1 .+ beta * U)));
     else
         B = (atan(beta * tan(pi * alpha / 2))) / alpha;
         S = (1 + beta ^ 2 * (tan(pi * alpha / 2))^2) ^ (1 / (2 * alpha));
         X = S * 
-            (sin.(alpha * (U + On * B))) ./ ((cos.(U)) .^ (1 / alpha)) .* 
-            ((cos.(U - alpha * (U + On * B))) ./ W) .^ ((1 - alpha) / alpha);
+            (sin.(alpha * (U .+ B))) ./ ((cos.(U)) .^ (1 / alpha)) .* 
+            ((cos.(U - alpha * (U .+ B))) ./ W) .^ ((1 - alpha) / alpha);
     end
     return X;
 end
 
 function alphastable(N, M, alpha, beta, gamma, delta, k)
     if k == 0
-        On = ones(N,M);
         if alpha == 1
-            X = gamma * alphastable_(N, M, alpha, beta) + On * delta;
+            X = gamma * alphastable_(N, M, alpha, beta) .+ delta;
         else
-            X = gamma * (alphastable_(N, M, alpha, beta) - On * beta * tan.(pi * alpha / 2)) + On * delta;
+            X = gamma * (alphastable_(N, M, alpha, beta) .- beta * tan.(pi * alpha / 2)) .+ delta;
         end
     else
-        On = ones(N,M);
         if alpha == 1
-            X = gamma * alphastable_(N, M, alpha, beta) + On * (delta + beta * 2 / pi *gamma * log(gamma));
+            X = gamma * alphastable_(N, M, alpha, beta) .+ (delta + beta * 2 / pi *gamma * log(gamma));
         else
-            X = gamma * alphastable_(N, M, alpha, beta) + On * delta;
+            X = gamma * alphastable_(N, M, alpha, beta) .+ delta;
         end
     end
     return X;
