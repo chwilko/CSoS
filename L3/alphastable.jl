65db6ec763f7eb18643af222bb91aeea75da4d54
Ryszard <ryszardrolik@gmail.com>
2022-03-18 22:23:38 +0100

    wychodzimy_z_julii_za_tydzien

--- L3/alphastable.jl
+++ L3/alphastable.jl
@@ -1,18 +1,30 @@
 include("MBP.jl")
 
-function alphastable_(N, M, alpha, beta)
+function alphastable_(N, M, alpha, beta, k)
     U = pi * (rand(N,M) .- (1 / 2));
     W = d_exp(N, M, 1);
-    if alpha == 1
-        X = 2 / pi * (
-            (pi / 2 .+ beta * U) .* tan.(U) -
-            beta * log.(W .* cos.(U) ./ (1 .+ beta * U)));
-    else
-        B = (atan(beta * tan(pi * alpha / 2))) / alpha;
-        S = (1 + beta ^ 2 * (tan(pi * alpha / 2))^2) ^ (1 / (2 * alpha));
-        X = S * 
-            (sin.(alpha * (U .+ B))) ./ ((cos.(U)) .^ (1 / alpha)) .* 
-            ((cos.(U - alpha * (U .+ B))) ./ W) .^ ((1 - alpha) / alpha);
+    if k == 0
+        if alpha == 1
+            X = 2 / pi * (
+                (pi / 2 .+ beta * U) .* tan.(U) -
+                beta * log.(W .* cos.(U) ./ (1 .+ (2 / pi) * beta * U)));
+        else
+            B = (atan(beta * tan(pi * alpha / 2))) / alpha;
+            S = (1 + beta ^ 2 * (tan(pi * alpha / 2))^2) ^ (1 / (2 * alpha));
+            X = S * 
+                ((sin.(alpha * (U .+ B))) ./ ((cos.(U)) .^ (1 / alpha))) .* 
+                ((cos.(U - alpha * (U .+ B))) ./ W) .^ ((1 - alpha) / alpha);
+        end
+    elseif k == 1
+        if alpha == 1
+            X = 2 / pi * (
+                (pi / 2 .+ beta * U) .* tan.(U) -
+                beta * log.(W .* cos.(U) ./ (1 .+ (2 / pi) * beta * U)));
+        else
+            B = (atan(beta * tan(pi * alpha / 2))) / alpha;
+            X = ((sin.(alpha * (U .+ B))) ./ ((cos.(alpha * B) .* cos.(U)) .^ (1 / alpha))) .* 
+                ((cos.(alpha * B .+ (alpha - 1) * U)) ./ W) .^ ((1 - alpha) / alpha);
+        end
     end
     return X;
 end
@@ -20,17 +32,16 @@ end
 function alphastable(N, M, alpha, beta, gamma, delta, k)
     if k == 0
         if alpha == 1
-            X = gamma * alphastable_(N, M, alpha, beta) .+ delta;
+            X = gamma * alphastable_(N, M, alpha, beta, k) .+ delta;
         else
-            X = gamma * (alphastable_(N, M, alpha, beta) .- beta * tan.(pi * alpha / 2)) .+ delta;
+            X = gamma * (alphastable_(N, M, alpha, beta, k) .- beta * tan.(pi * alpha / 2)) .+ delta;
         end
-    else
+    elseif k == 1
         if alpha == 1
-            X = gamma * alphastable_(N, M, alpha, beta) .+ (delta + beta * 2 / pi *gamma * log(gamma));
+            X = gamma * alphastable_(N, M, alpha, beta, k) .+ (delta + beta * (2 / pi) * gamma * log(gamma));
         else
-            X = gamma * alphastable_(N, M, alpha, beta) .+ delta;
+            X = gamma * alphastable_(N, M, alpha, beta, k) .+ delta;
         end
     end
     return X;
 end
-
--- packages/MBP.jl
+++ packages/MBP.jl
@@ -20,8 +20,6 @@ function CDF2(X)
     return (sum(X .< t' .* ones(MC,1), dims = 1) ./ (MC * ones(1,len)))';
 end
 
-
-
 function PDF(t, X, MC)
     t = t .- (t[2] - t[1]) / 2;
     p = CDF(t[1:end-1], X, MC);
