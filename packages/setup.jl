839c1cf3c0941e346f091cef28606e3d1e8c4487
chwilko <249761@stuent.pwr.edu.pl>
2022-03-18 22:23:40 +0100

    Niedziała

--- packages/setup.jl
+++ packages/setup.jl
@@ -2,9 +2,10 @@ import Pkg;
 Pkg.add("Plots");
 Pkg.add("Distributions");
 Pkg.add("PyPlot");
-Pkg.add("LaTeXStrings")
-Pkg.add("StatsPlots")
-Pkg.add("SpecialFunctions")
-Pkg.add("GLM")
-Pkg.add("DataFrames")
-Pkg.add("StatsBase")
+Pkg.add("LaTeXStrings");
+Pkg.add("StatsPlots");
+Pkg.add("SpecialFunctions");
+Pkg.add("GLM");
+Pkg.add("DataFrames");
+Pkg.add("StatsBase");
+Pkg.add("Optim");
