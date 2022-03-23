5edc6a8f841255037f82be9da586bc203ab7277a
chwilko <249761@stuent.pwr.edu.pl>
2022-03-23 15:29:10 +0100

    Zaczete zad listy 4

---
+++ Python3/packages_/multivariate_alphastable.py
@@ -0,0 +1,17 @@
+import numpy as np
+from alphastable import alphastable
+
+def multivariate_alphastable(alpha, gamma, points):
+    """function to simulate multivariate alpha stable variable
+    for the discrete spectral measure
+
+    Args:
+        gamma (_type_): _description_
+        points (_type_): _description_
+    """
+    if len(gamma) != len(points):
+        raise Exception("Vectors must have the same lengths!")
+    gamma = np.array(gamma).T
+    points = np.array(points)
+    Z = alphastable(len(gamma), 1, alpha, 1, 1, 0, 1)
+    return np.sum(gamma * Z * points, 0)
