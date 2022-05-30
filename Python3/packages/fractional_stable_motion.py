ecf90f4f5a5eca2433247fae9bedd5deab038b75
Ryszard <ryszardrolik@gmail.com>
2022-05-30 21:30:35 +0200

    changes to time horizon

--- Python3/packages/fractional_stable_motion.py
+++ Python3/packages/fractional_stable_motion.py
@@ -9,13 +9,13 @@ def f_levy(H, alpha, S, t, cut_point_1, S_negative, max_S_neg):
             (t - S_positive) ** (H - 1 / alpha))
         )
 
-def integral_form_simulation(H = 0.3, alpha = 2, beta = 0, M1 = -100, M2 = 100, N_trajectories = 1, I = 10000, dt = 0.01):
+def integral_form_simulation(H = 0.3, alpha = 2, beta = 0, M1 = -100, M2 = 100, N_trajectories = 1, I = 10000, dt = 0.01, time_horizon = 999):
     tau = (M2 - M1) / I
     S = M1 + np.arange(0, I + 1) * tau
     cut_point_1 = np.where(S > 0)[0][0] # changing sign
     S_negative = S[:cut_point_1 - 1]
     max_S_neg = np.maximum((-S_negative) ** (H - 1 / alpha), 0)
-    T = np.arange(0, dt * 999, dt)
+    T = np.arange(0, dt * time_horizon, dt)
     M = np.ones((len(T), N_trajectories))
     for i in range(N_trajectories):
         Z = alphastable(len(S), 1, alpha = alpha, beta = beta, gamma = tau ** (1 / alpha), delta = 0, k = 1)
